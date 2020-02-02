11af9d93093464054ca8b49bcc63ac8078e56e58
Mariusz Ratoń <raton.mariusz@gmail.com>
2020-02-02 19:40:23 +0100

    commit

---
+++ src/test/java/com/webapp/gameofrockpaperscissors/GameofrockpaperscissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.gameofrockpaperscissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameofrockpaperscissorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
